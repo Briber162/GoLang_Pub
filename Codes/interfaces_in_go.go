043f4381a37c4606a262ca698ed08d14039fcd61
KUNAL CHAUDHARY <briber162@gmail.com>
2023-09-30 12:31:31 +0530

    Update interfaces_in_go.go

--- Codes/interfaces_in_go.go
+++ Codes/interfaces_in_go.go
@@ -53,3 +53,50 @@ func main() {
 	fmt.Println(nsArea, nsP)
 
 }
+
+
+
+/* Example 2*/
+
+package main
+
+import "fmt"
+
+type shape interface {
+	area() float64
+	perim() float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (c circle) area() float64 {
+	return 3.14 * c.radius * c.radius
+}
+
+func (c circle) perim() float64 {
+	return 2 * 3.14 * c.radius
+}
+
+func callFunc(s shape) {
+	ans := s.area()
+	ans1 := s.perim()
+	fmt.Println(ans, ans1)
+}
+
+func main() {
+
+	myCircle := circle{
+		radius: 5.0,
+	}
+	callFunc(myCircle)
+
+}
+
+
+
+
+
+
+
